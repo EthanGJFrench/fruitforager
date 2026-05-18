eeed8128b8846057c3609f28f0a515350b9826d0
EthanGJFrench <ethanfrenc@gmail.com>
2026-05-18 17:43:25 +1200

    asaa

--- scripts/map.js
+++ scripts/map.js
@@ -2,7 +2,5 @@ var map = L.map('map').setView([-43.532, 172.636], 12);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 15,
-    attributionControl: false
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-
-// attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' required for lisencing
