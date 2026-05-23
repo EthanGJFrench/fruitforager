91d378d272eb58507301326483bf4cf70f27a0d6
EthanGJFrench <ethanfrenc@gmail.com>
2026-05-23 22:14:42 +1200

    fixed file path (it should now work in github pages

--- scripts/interactive_map_page/interactive_map.js
+++ scripts/interactive_map_page/interactive_map.js
@@ -132,7 +132,7 @@ export default class InteractiveMap {
         }
     
         const ICON = L.icon({ // close zoom
-            iconUrl: `../../assets/svgs/map_icons/${TREECOMMONNAME}.svg`,
+            iconUrl: `./assets/svgs/map_icons/${TREECOMMONNAME}.svg`,
             iconSize: [32, 32],
             iconAnchor: [16, 16]
         })
